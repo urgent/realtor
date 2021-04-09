6b5a4179cfd013ca1ac85eb0993d52a4f842a93f
urgent <urgent.programmer@gmail.com>
2021-04-08 21:45:20 -0400

    NextJS clean install

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>NJ Realtor</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </>
+  )
+}
