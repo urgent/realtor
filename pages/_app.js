e662b4f12b805d98456318cbdd1b9e7fc8a635f0
urgent <urgent.programmer@gmail.com>
2021-04-12 18:35:39 -0400

    #3 -- Removed app inline theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,6 @@
 import React from "react";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-
-// Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-const theme = extendTheme({ colors });
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
 
 function MyApp({ Component, pageProps }) {
   return (
